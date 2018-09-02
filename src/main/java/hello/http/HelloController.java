74c933b925a711c889c8effd0ff763e6ec9e8994
Shu Liu <shu.liu@bitmart.com>
2018-09-01 21:33:07 -0400

    test code

--- src/main/java/hello/http/HelloController.java
+++ src/main/java/hello/http/HelloController.java
@@ -4,26 +4,58 @@ import hello.service.GeoLocation;
 import hello.service.Coordinate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
+@RequestMapping("/")
 public class HelloController {
+
+
+    private final String secret = "newtonx-challenge";
 
     @RequestMapping(value= "/nxchallenge/astarisborn",
                     method = RequestMethod.GET,
                     produces = "application/json")
-    public String sayHello() {
+    @ResponseBody
+    public String query() {
 
-        GeoLocation gl = new GeoLocation();
-        String ip = gl.queryHostIP();
-        String hostIp = "\"myHostIP\"" + ":" + "\"" + ip + "\"";
+        return "\"response\"" + ":"+ "\"Hello World\"";
+    }
 
-        Coordinate c = gl.queryLocation(ip);
-        String latitude = "\"latitude\"" + ":" + "\"" + c.getLat() + "\"";
-        String longitude = "\"longitude\"" + ":" + "\"" + c.getLon() + "\"";
+    @RequestMapping(value= "/nxchallenge/astarisborn/{token}",
+            method = RequestMethod.GET,
+            produces = "application/json")
+    @ResponseBody
+    public String queryWithToken(@PathVariable("token") String token) {
 
-        return "\"response\"" + ":"+ "\"Hello World\"" + "\n\n" + hostIp + "\n" + latitude + "\n" + longitude;
+            if (!token.equals(secret)) {
+                return "the token is incorrect!!";
+            }
+
+            GeoLocation gl = new GeoLocation();
+            String ip = gl.queryHostIP();
+            String hostIp = "\"myHostIP\"" + ":" + "\"" + ip + "\"";
+
+            Coordinate c = gl.queryLocation(ip);
+            String latitude = "\"latitude\"" + ":" + "\"" + c.getLat() + "\"";
+            String longitude = "\"longitude\"" + ":" + "\"" + c.getLon() + "\"";
+
+            return "\"response\"" + ":"+ "\"Hello World\"" + "\n\n" + hostIp + "\n" + latitude + "\n" + longitude;
+    }
+
+    @RequestMapping("*")
+    @ResponseBody
+    public String fallbackMethod(){
+        return "you're lost... please query ./nxchallenge/astarisborn";
+    }
+
+    @RequestMapping()
+    @ResponseBody
+    public String defaultMethod(){
+        return "please query ./nxchallenge/astarisborn";
     }
 
     @RequestMapping("/healthz")
--- src/test/java/hello/HelloWorldConfigurationTests.java
+++ src/test/java/hello/HelloWorldConfigurationTests.java
@@ -17,34 +17,42 @@ package hello;
 
 import static org.junit.Assert.*;
 
+import hello.http.HelloController;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@DirtiesContext
+@AutoConfigureMockMvc
 public class HelloWorldConfigurationTests {
 
-    @LocalServerPort
-    private int port;
-
     @Autowired
-    private TestRestTemplate restTemplate;
+    private MockMvc mockMvc;
 
     @Test
-    public void testGreeting() throws Exception {
-        ResponseEntity<String> entity = restTemplate
-                .getForEntity("http://localhost:" + this.port + "/", String.class);
-//        assertEquals(HttpStatus.OK, entity.getStatusCode());
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/nxchallenge/astarisborn/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello World")));
     }
-
 }
